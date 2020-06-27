0693c6432f37ae5e6e5a25045c9bdce58fb85f66
Akihisa Shida <asihika0317@gmail.com>
2020-06-27 16:08:52 +0900

    Ra-men

---
+++ Ramen.Store/Ramen.Store/Pages/Success.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ramen.Store.Pages
+{
+    public class SuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+            var param = Request.Query["id"];
+            ViewData["param"] = param;
+        }
+    }
+}
