ba28e7b874efb4c2d6422717a4a34c426dacc27f
Akihisa Shida <asihika0317@gmail.com>
2020-06-27 17:07:25 +0900

    Changed

--- Ramen.Store/Ramen.Store/Pages/Success.cshtml.cs
+++ Ramen.Store/Ramen.Store/Pages/Success.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Stripe;
 
 namespace Ramen.Store.Pages
 {
@@ -11,8 +12,17 @@ namespace Ramen.Store.Pages
     {
         public void OnGet()
         {
-            var param = Request.Query["id"];
+            StripeConfiguration.ApiKey = Environment.GetEnvironmentVariable("StripeApiKey"); var options = new ProductListOptions
+            {
+                Limit = 3,
+            };
+            var service = new ProductService();
+            StripeList<Product> products = service.List(
+                options
+            ); var imageUrl = products.Data[0].Images[0]; var param = Request.Query["id"];
             ViewData["param"] = param;
+            ViewData["imageUrl"] = imageUrl;
         }
     }
 }
+
